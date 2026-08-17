c8297f2eaefba3462bfdaa3c47f82be3e44082c8
MJ06-afk <merryljordi@gmail.com>
2026-08-17 17:56:25 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ const AuthenticatedApp = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/grillz-sur-mesure" element={<Grillz />} />
+      <Route path="/catalogue" element={<Catalogue />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
